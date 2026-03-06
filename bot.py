9c52ea099a11c7d88e5db9a7d8efa7c6073c5675
Alan553 <40385807+Alan553@users.noreply.github.com>
2026-03-07 05:28:49 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,104 @@
+from telethon import TelegramClient, events
+import re
+import os
+from datetime import datetime
+
+api_id = int(os.getenv("39780948"))
+api_hash = os.getenv("be29273ba2666a856a477c219b800b6f")
+
+client = TelegramClient("bot", api_id, api_hash)
+
+channels = [
+    "financialjuice",
+    "marketfeed"
+]
+
+bullish_words = [
+    "rate cut",
+    "dovish",
+    "recession",
+    "weak data",
+    "cpi lower",
+    "unemployment rise"
+]
+
+bearish_words = [
+    "rate hike",
+    "hawkish",
+    "strong jobs",
+    "cpi higher",
+    "inflation rising",
+    "economy strong"
+]
+
+high_impact = [
+    "CPI",
+    "NFP",
+    "FOMC",
+    "Powell",
+    "Fed Chair",
+    "Interest Rate",
+    "Rate Decision"
+]
+
+def analyze_news(text):
+
+    score = 0
+
+    for word in bullish_words:
+        if word.lower() in text.lower():
+            score += 1
+
+    for word in bearish_words:
+        if word.lower() in text.lower():
+            score -= 1
+
+    if score > 0:
+        signal = "BUY GOLD"
+    elif score < 0:
+        signal = "SELL GOLD"
+    else:
+        signal = "NEUTRAL"
+
+    confidence = min(abs(score) * 25, 90)
+
+    return signal, confidence
+
+
+@client.on(events.NewMessage(chats=channels))
+async def handler(event):
+
+    text = event.raw_text
+
+    signal, confidence = analyze_news(text)
+
+    impact = "LOW"
+
+    for word in high_impact:
+        if word.lower() in text.lower():
+            impact = "HIGH"
+
+    if impact == "HIGH":
+
+        print("")
+
+        print("🚨 HIGH IMPACT NEWS DETECTED")
+        print(text)
+
+        print("")
+        print("📊 SIGNAL :", signal)
+        print("🎯 CONFIDENCE :", confidence,"%")
+        print("⏰ TIME :", datetime.now())
+
+        print("")
+
+
+print("AI NEWS SNIPER GOLD BOT RUNNING...")
+
+client.start()
+client.run_until_disconnected()
+
+
+
+API_ID=39780948
+API_HASH=be29273ba2666a856a477c219b800b6f
